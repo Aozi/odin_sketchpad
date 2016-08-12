b4cabec91b263398cf6f6cd6ffd3a25a13a2ceb3
Aozi <niko.novitsky@gmail.com>
2016-08-12 19:46:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,5 +18,17 @@ $(document).ready(function () {
 		}
 
 		$('.square').mouseenter(function() {
-				$(this).css("background-color","black")})
+    if(!this.style.backgroundColor) {
+    	$(this).css("background-color",getRandomColor())}
+    }
+	)
 });
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
